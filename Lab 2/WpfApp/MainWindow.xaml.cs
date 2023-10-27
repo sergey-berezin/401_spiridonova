2622ecee4437b8c61814eb0d082e5f2b9d120cca
AlinaStudies <91690629+AlinaStudies@users.noreply.github.com>
2023-10-27 23:07:56 +0300

    Add files via upload

---
+++ Lab 2/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using ViewModel;
+
+namespace Lab2
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainViewModel viewModel = new();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = viewModel;
+        }
+    }
+}
