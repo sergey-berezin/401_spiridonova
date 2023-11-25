e20101580100b2d3b5bf6bbde241d9a934f38067
AlinaStudies <91690629+AlinaStudies@users.noreply.github.com>
2023-11-26 00:35:57 +0300

    Add files via upload

---
+++ Lab 3/DataStorageTools/Migrations/20231123192137_Initial.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataStorageTools.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProcessedImages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ImagePath = table.Column<string>(type: "TEXT", nullable: false),
+                    Image = table.Column<byte[]>(type: "BLOB", nullable: false),
+                    Width = table.Column<int>(type: "INTEGER", nullable: false),
+                    Height = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProcessedImages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DetectedObjects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    XMin = table.Column<double>(type: "REAL", nullable: false),
+                    YMin = table.Column<double>(type: "REAL", nullable: false),
+                    XMax = table.Column<double>(type: "REAL", nullable: false),
+                    YMax = table.Column<double>(type: "REAL", nullable: false),
+                    Confidence = table.Column<double>(type: "REAL", nullable: false),
+                    Class = table.Column<string>(type: "TEXT", nullable: false),
+                    ProcessedImageId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DetectedObjects", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DetectedObjects_ProcessedImages_ProcessedImageId",
+                        column: x => x.ProcessedImageId,
+                        principalTable: "ProcessedImages",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DetectedObjects_ProcessedImageId",
+                table: "DetectedObjects",
+                column: "ProcessedImageId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DetectedObjects");
+
+            migrationBuilder.DropTable(
+                name: "ProcessedImages");
+        }
+    }
+}
